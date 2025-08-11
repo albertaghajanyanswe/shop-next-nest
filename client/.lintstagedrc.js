7dde0b9989bc269619bb366addb00946151c2a28
Albert Aghajanyan <albert.aghajanyan.swe@gmail.com>
2025-08-11 18:37:15 +0400

    Test

--- client/.lintstagedrc.js
+++ client/.lintstagedrc.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames
+  `eslint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`;
+    .join(' --file ')}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'],
-  '*.{json,css,md}': ['prettier --write'],
+  '**/*.{js,jsx,ts,tsx}': [buildEslintCommand],
 };
