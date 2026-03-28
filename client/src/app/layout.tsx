46ef292f325e06a02633fe92e5619041e65a497f
Albert Aghajanyan <albert.aghajanyan.swe@gmail.com>
2026-03-28 17:45:09 +0400

    Test deploy

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Poppins } from 'next/font/google';
 const poppins = Poppins({
   subsets: ['latin'],
   style: 'normal',
-  weight: ['400', '500', '600', '700', '800'],
+  weight: ['400', '500', '600', '700'],
   display: 'swap',
 });
 
