22b11b42b7481c40d63181deb9ce981e9ec2b6de
Albert Aghajanyan <albert.aghajanyan.swe@gmail.com>
2026-05-22 16:23:00 +0400

    Update home banner image

--- client/src/components/customComponents/homeBanner/HomeBanner.tsx
+++ client/src/components/customComponents/homeBanner/HomeBanner.tsx
@@ -16,7 +16,8 @@ function HomeBannerComponent() {
           <div className='-ml-4 flex'>
             <div className='relative h-auto max-h-[450px] min-h-[300px] w-full min-w-0 shrink-0 grow-0 basis-full px-0 lg:min-h-[450px]'>
               <Image
-                src='https://res.cloudinary.com/dvuo50sjj/image/upload/w_2000,f_auto,q_auto/v1764674699/banner_wmezby.png'
+                // src='https://res.cloudinary.com/dvuo50sjj/image/upload/w_2000,f_auto,q_auto/v1764674699/banner_wmezby.png'
+                src='/banner/banner-1.png'
                 className='h-full w-full object-cover opacity-90'
                 width={2000}
                 height={350}
@@ -29,11 +30,11 @@ function HomeBannerComponent() {
                 <div className='relative mx-auto h-full max-w-screen-xl px-4'>
                   <div className='global-container relative h-full content-center space-y-5 py-10 sm:py-15 lg:space-y-10 lg:py-20'>
                     <div className='flex max-w-full flex-col gap-4 sm:max-w-[60%]'>
-                      <h1 className='text-shop-primary text-xl font-semibold sm:text-2xl lg:text-3xl'>
+                      <h1 className='text-black text-xl font-semibold sm:text-2xl lg:text-3xl'>
                         {t('banner_title')}
                       </h1>
                       <Link href={PUBLIC_URL.shop()}>
-                        <Button variant='primary' className='group'>
+                        <Button variant='outline' className='group'>
                           {t('banner_button')}
                           <ArrowRight className='ml-[4px] size-4 transition-all group-hover:ml-[5px]' />
                         </Button>
